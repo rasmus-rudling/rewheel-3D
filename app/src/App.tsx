141b80f7db0c7ab057c2aa57002946193447d2ba
Rasmus Rudling <rasmusrudling@gmail.com>
2021-10-05 18:13:25 +0200

    feat: spinner

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,21 @@
-import BikeBuilder from "./components/pages/BikeBuilder/BikeBuilder";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ProfilePage from './components/pages/Profile/ProfilePage';
+import BikeBuilderPage from './components/pages/BikeBuilder/BikeBuilderPage';
 
 const App = () => {
-  return (
-    <>
-      <BikeBuilder />
-    </>
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					<BikeBuilderPage />
+				</Route>
+
+				<Route exact path="/profile">
+					<ProfilePage />
+				</Route>
+			</Switch>
+		</Router>
+	);
 };
 
 export default App;
