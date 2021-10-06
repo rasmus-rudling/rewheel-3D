2b4fb05dfd8fa4f86b7d18408ed0da0aa4798b88
Gnabriel <ls6l9g7g9ud1@opayq.com>
2021-10-06 15:50:20 +0200

    Added user signup, login and authentication

--- backend/src/resolvers.js
+++ backend/src/resolvers.js
@@ -1,8 +1,16 @@
+require("dotenv").config();
+import { UserInputError, AuthenticationError } from "apollo-server-errors";
+import jwt from "jsonwebtoken";
 import { Bike } from "./models/Bike";
-import { User } from ".models/User";
+import User from "./models/User";
+
+const jwt_secret = process.env.JWT_SECRET;
 
 export const resolvers = {
   Query: {
+    getCurrentUser: (root, args, context) => {
+      return context.currentUser;
+    },
     getAllBikes: async () => await Bike.find(),
   },
   Mutation: {
@@ -15,7 +23,29 @@ export const resolvers = {
         });
       });
     },
-    addBike: async (_, { color }) => {
+    login: async (root, args) => {
+      const user = await User.findOne({
+        username: args.username,
+        password: args.password,
+      });
+
+      if (!user) {
+        throw new UserInputError("Wrong login credentials.");
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      };
+
+      return { value: jwt.sign(userForToken, jwt_secret) };
+    },
+    addBike: async (root, { color }, context) => {
+      // Check if user is logged in.
+      const currentUser = context.currentUser;
+      if (!currentUser) {
+        throw new AuthenticationError("Not authenticated.");
+      }
       const newBike = new Bike({ color });
       await newBike.save();
       return newBike;
--- backend/src/typeDefs.js
+++ backend/src/typeDefs.js
@@ -1,14 +1,25 @@
 import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
-  type Query {
-    getAllBikes: [Bike!]!
+  type User {
+    id: ID!
+    username: String!
+    password: String!
+  }
+  type Token {
+    value: String!
   }
   type Bike {
     id: ID!
     color: String!
   }
+  type Query {
+    getCurrentUser: User
+    getAllBikes: [Bike!]!
+  }
   type Mutation {
+    createUser(username: String!, password: String!): User
+    login(username: String!, password: String!): Token
     addBike(color: String!): Bike!
   }
 `;
