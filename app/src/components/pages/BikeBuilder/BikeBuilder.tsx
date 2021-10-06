74c27207dc1e86524eed2b03fb83e281bfb46819
unknown <gabriel.rosenberg@outlook.com>
2021-10-06 16:13:55 +0200

    working on gltf

--- app/src/components/pages/BikeBuilder/BikeBuilder.tsx
+++ app/src/components/pages/BikeBuilder/BikeBuilder.tsx
@@ -1,11 +1,14 @@
 import SidePanel from "./SidePanel";
+import BikeView from "./BikeView";
 
 const BikeBuilder = () => {
   return (
     <div className="App">
       <nav className="w-full h-14 bg-purple-400 fixed top-0"></nav>
       <div className="flex items-stretch pt-14 h-full">
-        <div className="flex-grow"></div>
+        <div className="flex-grow">
+          <BikeView />
+        </div>
         <SidePanel />
       </div>
     </div>
