4d3b774830e0957877e6653c3956b021326c9d99
Gabriel Rosenberg <rosenberg.gabriel@outlook.com>
2021-10-07 16:52:14 +0200

    Structuring the builder component

--- app/src/components/pages/BikeBuilder/BikeView.tsx
+++ app/src/components/pages/BikeBuilder/BikeView.tsx
@@ -1,24 +1,61 @@
-import React, { useRef, Suspense } from "react";
+import React, { useRef, Suspense, useState } from "react";
 import * as THREE from "three";
 import { useGLTF, OrbitControls } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { GLTF } from "three/examples/jsm/loaders/GLTFLoader";
 
 import BikeFrameModel from "./../../../resources/testGeometry/bikeFrame.gltf";
+import { Group } from "three";
 
 type GLTFResult = GLTF & {
   nodes: {
     Cylinder: THREE.Mesh;
+    [key: string]: THREE.Object3D;
   };
   materials: {};
 };
 
+enum Component {
+  FRAME,
+  HANDLEBAR,
+  FRONTWHEEL,
+  BACKWHEEL,
+  SEATPOST,
+}
+interface Anchor {
+  position: THREE.Vector3;
+  rotation: THREE.Vector3;
+}
+interface Anchors {
+  [key: string]: Anchor;
+}
+interface ComponentConfig {
+  id: number;
+  type: Component;
+  anchors: Anchors;
+}
+
 const BikeFrame = (props: JSX.IntrinsicElements["group"]) => {
   const group = useRef<THREE.Group>();
   const { nodes, materials } = useGLTF(BikeFrameModel) as GLTFResult;
+  for (const item in nodes) {
+    console.log(nodes)
+    //if (item.type === "Object3D") console.log(nodes[key]);
+
+    return (
+      <mesh
+        name="bikeFrame"
+        castShadow
+        receiveShadow
+        geometry={nodes.Cylinder.geometry}
+        material={nodes.Cylinder.material}
+      />
+    );
+  }
+
   return (
     <group ref={group} {...props} dispose={null}>
-      <group name="Scene" position={[0, -0.3, 0]}>
+      {/* <group name="Scene" position={[0, -0.3, 0]}>
         <group name="frontWheelAnchor" position={[-1.64, 0.17, 0]} />
         <group name="backWheelAnchor" position={[1.1, 0.07, 0]} />
         <group
@@ -27,7 +64,7 @@ const BikeFrame = (props: JSX.IntrinsicElements["group"]) => {
           rotation={[0, 0, -0.31]}
         />
         <group
-          name="stearingAnchor"
+          name="handlebarAnchor"
           position={[-1.05, 1.44, 0]}
           rotation={[0, 0, -0.39]}
         />
@@ -38,12 +75,22 @@ const BikeFrame = (props: JSX.IntrinsicElements["group"]) => {
           geometry={nodes.Cylinder.geometry}
           material={nodes.Cylinder.material}
         />
-      </group>
+      </group> */}
     </group>
   );
 };
 
 useGLTF.preload(BikeFrameModel);
+
+const BikeBuild = () => {
+  const [frameConfig, setframeConfig] = useState({} as ComponentConfig);
+  const [seatPostConfig, setSeatPostConfig] = useState({} as ComponentConfig);
+  const [handlebarConfig, sethandlebarConfig] = useState({} as ComponentConfig);
+  const [backWheelConfig, setBackWheelConfig] = useState({} as ComponentConfig);
+  const [frontWheelConfig, setFrontWheelConfig] = useState({} as ComponentConfig);
+
+  return <BikeFrame />;
+};
 
 const BikeView = () => {
   return (
@@ -51,9 +98,14 @@ const BikeView = () => {
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
       <Suspense fallback={null}>
-        <BikeFrame />
+        <BikeBuild />
       </Suspense>
-      <OrbitControls enablePan={false} enableZoom={false} minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} />
+      <OrbitControls
+        enablePan={false}
+        enableZoom={false}
+        minPolarAngle={Math.PI / 2}
+        maxPolarAngle={Math.PI / 2}
+      />
     </Canvas>
   );
 };
