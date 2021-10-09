6cf153ffceb4d408d44858fced25e4eb8f3d6d58
Gabriel Rosenberg <rosenberg.gabriel@outlook.com>
2021-10-09 10:47:42 +0200

    Working on model loader

--- app/src/components/pages/BikeBuilder/BikeView.tsx
+++ app/src/components/pages/BikeBuilder/BikeView.tsx
@@ -1,95 +1,104 @@
-import React, { useRef, Suspense, useState } from "react";
+import React, { useRef, Suspense, useState, useEffect } from "react";
 import * as THREE from "three";
 import { useGLTF, OrbitControls } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { GLTF } from "three/examples/jsm/loaders/GLTFLoader";
 
 import BikeFrameModel from "./../../../resources/testGeometry/bikeFrame.gltf";
-import { Group } from "three";
+import { BufferGeometry, Group } from "three";
+import { JsxElement } from "typescript";
 
 type GLTFResult = GLTF & {
   nodes: {
-    Cylinder: THREE.Mesh;
     [key: string]: THREE.Object3D;
   };
   materials: {};
 };
 
-enum Component {
-  FRAME,
-  HANDLEBAR,
-  FRONTWHEEL,
-  BACKWHEEL,
-  SEATPOST,
-}
-interface Anchor {
+type BikeCompProps = JSX.IntrinsicElements["group"] & {
+  config: ComponentConfig;
+  setConfig: React.Dispatch<React.SetStateAction<ComponentConfig>>;
+};
+
+type Anchor = {
   position: THREE.Vector3;
-  rotation: THREE.Vector3;
-}
-interface Anchors {
+  rotation: THREE.Euler;
+};
+
+type Anchors = {
   [key: string]: Anchor;
-}
-interface ComponentConfig {
+};
+
+type ComponentConfig = {
   id: number;
-  type: Component;
+  source: string;
+  visible: boolean;
+  type: string;
   anchors: Anchors;
-}
+};
 
-const BikeFrame = (props: JSX.IntrinsicElements["group"]) => {
+const BikeFrame = (props: BikeCompProps) => {
   const group = useRef<THREE.Group>();
-  const { nodes, materials } = useGLTF(BikeFrameModel) as GLTFResult;
-  for (const item in nodes) {
-    console.log(nodes)
-    //if (item.type === "Object3D") console.log(nodes[key]);
+  const { scene } = useGLTF(BikeFrameModel) as GLTF;
 
-    return (
-      <mesh
-        name="bikeFrame"
-        castShadow
-        receiveShadow
-        geometry={nodes.Cylinder.geometry}
-        material={nodes.Cylinder.material}
-      />
-    );
-  }
+  const bikeComponentConfig = {} as ComponentConfig;
+  const anchors: Anchors = {};
+  const children: any = scene.children;
+
+  let geometry = {} as BufferGeometry;
+  useEffect(() => {
+    for (const idx in scene.children) {
+      const objectName = scene.children[idx].name;
+      const objectType = scene.children[idx].type;
+
+      if (objectType === "Object3D") {
+        anchors[objectName] = {
+          position: children[idx].position,
+          rotation: children[idx].rotation,
+        } as Anchor;
+      } else if (objectType === "Mesh") {
+        bikeComponentConfig.type = objectType;
+        geometry = children[idx].geometry;
+      }
+    }
+
+    bikeComponentConfig.anchors = anchors;
+    props.setConfig(bikeComponentConfig);
+  }, []);
 
   return (
-    <group ref={group} {...props} dispose={null}>
-      {/* <group name="Scene" position={[0, -0.3, 0]}>
-        <group name="frontWheelAnchor" position={[-1.64, 0.17, 0]} />
-        <group name="backWheelAnchor" position={[1.1, 0.07, 0]} />
-        <group
-          name="seatPostAnchor"
-          position={[0.45, 1.39, 0]}
-          rotation={[0, 0, -0.31]}
-        />
-        <group
-          name="handlebarAnchor"
-          position={[-1.05, 1.44, 0]}
-          rotation={[0, 0, -0.39]}
-        />
-        <mesh
-          name="bikeFrame"
-          castShadow
-          receiveShadow
-          geometry={nodes.Cylinder.geometry}
-          material={nodes.Cylinder.material}
-        />
-      </group> */}
-    </group>
+    <mesh
+      name={bikeComponentConfig.type}
+      ref={group}
+      castShadow
+      receiveShadow
+      geometry={geometry}
+    />
   );
 };
 
 useGLTF.preload(BikeFrameModel);
 
 const BikeBuild = () => {
-  const [frameConfig, setframeConfig] = useState({} as ComponentConfig);
+  const [frameConfig, setFrameConfig] = useState({} as ComponentConfig);
   const [seatPostConfig, setSeatPostConfig] = useState({} as ComponentConfig);
   const [handlebarConfig, sethandlebarConfig] = useState({} as ComponentConfig);
   const [backWheelConfig, setBackWheelConfig] = useState({} as ComponentConfig);
-  const [frontWheelConfig, setFrontWheelConfig] = useState({} as ComponentConfig);
+  const [frontWheelConfig, setFrontWheelConfig] = useState(
+    {} as ComponentConfig
+  );
 
-  return <BikeFrame />;
+  const bikeComponents: JSX.Element[] = [];
+
+  const displayBikeComponent = () => {
+    bikeComponents.push(
+      <BikeFrame config={frameConfig} setConfig={setFrameConfig} />
+    );
+  };
+
+  displayBikeComponent();
+
+  return <group name="bikeBuild">{bikeComponents.map((e) => e)}</group>;
 };
 
 const BikeView = () => {
