653a5aa2db4d0ab954cc3e9a0b66b385b8409bb9
Isak Pettersson <isak.movitz.pettersson@gmail.com>
2021-10-15 14:25:54 +0200

    added basic skeleton for discover page

--- app/src/components/common/NavBar/NavBarPresenter.tsx
+++ app/src/components/common/NavBar/NavBarPresenter.tsx
@@ -1,34 +1,35 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router';
-import DesktopNavBarView from './NavBarView';
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+import DesktopNavBarView from "./NavBarView";
 
 export interface NavButton {
-	route: string;
-	text: string;
+  route: string;
+  text: string;
 }
 
 const navButtons: NavButton[] = [
-	{ route: 'bike-builder', text: 'cykelbyggaren' },
-	{ route: 'profile', text: 'profil' },
-	{ route: 'login', text: 'logga in' },
+  { route: "discover", text: "utforska" },
+  { route: "bike-builder", text: "cykelbyggaren" },
+  { route: "profile", text: "profil" },
+  { route: "login", text: "logga in" },
 ];
 
 const NavBarPresenter = () => {
-	const history = useHistory();
+  const history = useHistory();
 
-	const navButtonClickHandler = (newRoute: string) => {
-		history.push(newRoute);
-	};
+  const navButtonClickHandler = (newRoute: string) => {
+    history.push(newRoute);
+  };
 
-	return (
-		<>
-			<DesktopNavBarView
-				navButtons={navButtons}
-				navButtonClickHandler={navButtonClickHandler}
-			/>
-			{/* <MobileNavBar navigationButtons={navigationButtons} /> */}
-		</>
-	);
+  return (
+    <>
+      <DesktopNavBarView
+        navButtons={navButtons}
+        navButtonClickHandler={navButtonClickHandler}
+      />
+      {/* <MobileNavBar navigationButtons={navigationButtons} /> */}
+    </>
+  );
 };
 
 export default NavBarPresenter;
