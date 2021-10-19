edbc9acfa837f05c54024664eef29826567991a2
Isak Pettersson <isak.movitz.pettersson@gmail.com>
2021-10-19 10:01:27 +0200

    added basic slider structure

--- app/src/components/common/buttons/Button1View.tsx
+++ app/src/components/common/buttons/Button1View.tsx
@@ -1,96 +1,99 @@
 interface Props {
-	text: string;
-	onClickHandler: () => void;
-	color: 'blue' | 'green' | 'red' | 'gray';
-	type?: 'submit' | 'button';
-	addBorder?: boolean;
-	blackTextColor?: boolean;
-	filled?: boolean;
-	extraClass?: string;
+  text: string;
+  onClickHandler: () => void;
+  disabled?: boolean;
+  color: "blue" | "green" | "red" | "gray";
+  type?: "submit" | "button";
+  addBorder?: boolean;
+  blackTextColor?: boolean;
+  filled?: boolean;
+  extraClass?: string;
 }
 
 const Button1 = ({
-	text,
-	onClickHandler,
-	color,
-	type = 'button',
-	addBorder = false,
-	blackTextColor = true,
-	filled = false,
-	extraClass = '',
+  text,
+  onClickHandler,
+  disabled = false,
+  color,
+  type = "button",
+  addBorder = false,
+  blackTextColor = true,
+  filled = false,
+  extraClass = "",
 }: Props) => {
-	let colorStyling = '',
-		borderStyling = '',
-		fontColor = '';
+  let colorStyling = "",
+    borderStyling = "",
+    fontColor = "";
 
-	if (filled) {
-		fontColor = 'text-white';
+  if (filled) {
+    fontColor = "text-white";
 
-		if (color === 'blue') {
-			colorStyling = 'bg-blue-500 hover:bg-blue-400';
-		} else if (color === 'green') {
-			colorStyling = 'bg-green-500 hover:bg-green-400';
-		} else if (color === 'red') {
-			colorStyling = 'bg-red-500 hover:bg-red-400';
-		} else {
-			colorStyling = 'bg-gray-500 hover:bg-gray-400';
-		}
-	} else {
-		if (color === 'blue') {
-			colorStyling = 'hover:bg-blue-300';
-			fontColor = 'text-blue-500';
-			borderStyling = 'border border-blue-500';
-		} else if (color === 'green') {
-			colorStyling = 'hover:bg-green-300';
-			fontColor = 'text-green-500';
-			borderStyling = 'border border-green-500';
-		} else if (color === 'red') {
-			colorStyling = 'hover:bg-red-300';
-			fontColor = 'text-red-500';
-			borderStyling = 'border border-red-500';
-		} else {
-			colorStyling = 'hover:bg-gray-300';
-			fontColor = 'text-gray-500';
-			borderStyling = 'border border-gray-500';
-		}
-	}
+    if (color === "blue") {
+      colorStyling = "bg-blue-500 hover:bg-blue-400";
+    } else if (color === "green") {
+      colorStyling = "bg-green-500 hover:bg-green-400";
+    } else if (color === "red") {
+      colorStyling = "bg-red-500 hover:bg-red-400";
+    } else {
+      colorStyling = "bg-gray-500 hover:bg-gray-400";
+    }
+  } else {
+    if (color === "blue") {
+      colorStyling = "hover:bg-blue-300";
+      fontColor = "text-blue-500";
+      borderStyling = "border border-blue-500";
+    } else if (color === "green") {
+      colorStyling = "hover:bg-green-300";
+      fontColor = "text-green-500";
+      borderStyling = "border border-green-500";
+    } else if (color === "red") {
+      colorStyling = "hover:bg-red-300";
+      fontColor = "text-red-500";
+      borderStyling = "border border-red-500";
+    } else {
+      colorStyling = "hover:bg-gray-300";
+      fontColor = "text-gray-500";
+      borderStyling = "border border-gray-500";
+    }
+  }
 
-	if (!addBorder) {
-		borderStyling = '';
-	}
+  if (!addBorder) {
+    borderStyling = "";
+  }
 
-	if (blackTextColor) {
-		fontColor = 'text-gray-900';
-	}
+  if (blackTextColor) {
+    fontColor = "text-gray-900";
+  }
 
-	const basicStyle = `
-        capitalize font-light 
-        rounded-sm 
-        h-8 px-3 mr-2
-        last:mr-0
+  const basicStyle = `
+		  capitalize font-light 
+		  rounded-sm 
+		  h-8 px-3 mr-2
+		  last:mr-0
+  
+		  transition
+		  duration-200
+		  select-none
+	  `;
 
-        transition
-        duration-200
-        select-none
-    `;
+  const allStyling = [
+    basicStyle,
+    colorStyling,
+    borderStyling,
+    fontColor,
+    extraClass,
+  ];
 
-	const allStyling = [
-		basicStyle,
-		colorStyling,
-		borderStyling,
-		fontColor,
-		extraClass,
-	];
-
-	return (
-		<button
-			onClick={onClickHandler}
-			className={allStyling.join(' ')}
-			type={type}
-		>
-			{text}
-		</button>
-	);
+  return (
+    <button
+      onClick={onClickHandler}
+      className={allStyling.join(" ")}
+      type={type}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button1;
--- app/src/components/pages/Discover/DiscoverPage.tsx
+++ app/src/components/pages/Discover/DiscoverPage.tsx
@@ -1,11 +1,84 @@
-import React from "react";
-import Spinner from "../../common/Spinner";
+import React, { Component, useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+import Button1 from "../../common/buttons/Button1View";
+
+const data = {
+  properties: [
+    {
+      index: 0,
+      color: "red",
+    },
+    {
+      index: 1,
+      color: "green",
+    },
+    {
+      index: 2,
+      color: "yellow",
+    },
+    {
+      index: 3,
+      color: "orange",
+    },
+    {
+      index: 4,
+      color: "purple",
+    },
+    {
+      index: 5,
+      color: "blue",
+    },
+    {
+      index: 6,
+      color: "gray",
+    },
+  ],
+};
+
+var numberOfObjects = data.properties.length;
+
+function Slides() {
+  const [index, setIndex] = useState(0);
+
+  const nextProperty = () => {
+    setIndex(index + 1);
+    console.log(index);
+  };
+
+  const prevProperty = () => {
+    setIndex(index - 1);
+    console.log(index);
+  };
+
+  return (
+    <div>
+      <Button1
+        color="gray"
+        onClickHandler={() => prevProperty()}
+        disabled={index === 0}
+        text="Prev"
+        addBorder={true}
+      />
+
+      <Button1
+        color="gray"
+        onClickHandler={() => nextProperty()}
+        disabled={index === numberOfObjects - 1}
+        text="Next"
+        addBorder={true}
+      />
+
+      <div> {data.properties[index].color}</div>
+    </div>
+  );
+}
 
 const DiscoverPage = () => {
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <div className="flex justify-center h-max w-max">
-        <Spinner />
+        <Slides></Slides>
       </div>
     </div>
   );
