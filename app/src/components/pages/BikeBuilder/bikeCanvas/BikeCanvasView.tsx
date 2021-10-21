e55d76dfe2fafc9988e209041b6afdf95f84245e
Gnabriel <elleb@kth.se>
2021-10-21 16:54:27 +0200

    Enabled environment light in suspense

--- app/src/components/pages/BikeBuilder/bikeCanvas/BikeCanvasView.tsx
+++ app/src/components/pages/BikeBuilder/bikeCanvas/BikeCanvasView.tsx
@@ -32,15 +32,10 @@ const BikeView = ({ bikeConfig }: Props) => {
         <pointLight position={[-1.16, 1.262, -2.143]} castShadow={true} />
         <pointLight position={[0.442, 0.868, 1.574]} castShadow={true} />
         <pointLight position={[-2.427, 2.07, 1.574]} castShadow={true} />
-        {/* <Plane
-          receiveShadow={true}
-          material={new THREE.MeshStandardMaterial()}
-          scale={new THREE.Vector3(4, 4, 4)}
-          rotation={new THREE.Euler(-Math.PI / 2, 0, 0)}
-          position={new THREE.Vector3(0, -1.2, 0)}
-        /> */}
-        <Suspense fallback={<div>Loading... </div>}>
+
+        <Suspense fallback={""}>
           <ComposedBikeBuild bikeConfig={bikeConfig} />
+          <Environment preset="city" />
         </Suspense>
 
         <OrbitControls
@@ -49,7 +44,7 @@ const BikeView = ({ bikeConfig }: Props) => {
           minPolarAngle={Math.PI / 2.5}
           maxPolarAngle={Math.PI / 2.5}
         />
-        {/* <Environment preset="city" /> Not used since causes error in discover.*/}
+
         <ContactShadows
           rotation-x={Math.PI / 2}
           position={[0, -1.2, 0]}
@@ -60,7 +55,10 @@ const BikeView = ({ bikeConfig }: Props) => {
           far={4.5}
         />
       </Canvas>
-      <img className="z-0 h-full mx-auto absolute inset-0" src={BikePodium} />
+      <img
+        className="z-0 h-full mx-auto absolute inset-0 select-none"
+        src={BikePodium}
+      />
     </div>
   );
 };
