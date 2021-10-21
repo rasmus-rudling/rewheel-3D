12ed94e54644a0fd595e8be28cf8f71c33185a5f
Gnabriel <elleb@kth.se>
2021-10-21 16:18:34 +0200

    Added svg background for bike canvas

--- app/src/components/pages/BikeBuilder/bikeCanvas/BikeCanvasView.tsx
+++ app/src/components/pages/BikeBuilder/bikeCanvas/BikeCanvasView.tsx
@@ -1,41 +1,51 @@
-import React, { useRef, Suspense, useState, useEffect } from 'react';
-import { useGLTF, OrbitControls } from '@react-three/drei';
-import { Canvas, useFrame } from '@react-three/fiber';
-
-import ComposedBikeBuild from './bikeBuild/BikeBuildPresenter';
-
-import { BikeConfig } from '../../../../types';
+import React, { useRef, Suspense, useState, useEffect } from "react";
+import { useGLTF, OrbitControls, Plane, Circle } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import ComposedBikeBuild from "./bikeBuild/BikeBuildPresenter";
+import { BikeConfig } from "../../../../types";
+import BikePodium from "../../../../resources/images/bike_podium.svg";
+import * as THREE from "three";
 
 type Props = {
-	// Can't be interface since base type includes optinal arguments
-	bikeConfig: BikeConfig;
+  // Can't be interface since base type includes optinal arguments
+  bikeConfig: BikeConfig;
 };
 
 const BikeView = ({ bikeConfig }: Props) => {
-	// const currentBikeBuild = useCurrentBuild();
+  // const currentBikeBuild = useCurrentBuild();
 
-	return (
-		<Canvas
-			dpr={[1, 2]}
-			camera={{ position: [-4, 2, -4], fov: 45 }}
-			className="bg-gray-400"
-		>
-			<ambientLight intensity={0.5} />
-			<pointLight position={[-1.16, 1.262, -2.143]} />
-			<pointLight position={[0.442, 0.868, 1.574]} />
-			<pointLight position={[-2.427, 2.07, 1.574]} />
+  return (
+    <div className="h-full w-full bg-gray-300">
+      <Canvas
+        dpr={[1, 2]}
+        camera={{ position: [-4, 2, -4], fov: 50 }}
+        className="z-10"
+      >
+        <ambientLight intensity={0.5} />
+        <pointLight position={[-1.16, 1.262, -2.143]} />
+        <pointLight position={[0.442, 0.868, 1.574]} />
+        <pointLight position={[-2.427, 2.07, 1.574]} />
+        {/* <Plane
+          receiveShadow={true}
+          material={new THREE.MeshStandardMaterial()}
+          scale={new THREE.Vector3(4, 4, 4)}
+          rotation={new THREE.Euler(-Math.PI / 2, 0, 0)}
+          position={new THREE.Vector3(0, -1.2, 0)}
+        /> */}
 
-			<Suspense fallback={<div>Loading... </div>}>
-				<ComposedBikeBuild bikeConfig={bikeConfig} />
-			</Suspense>
-			<OrbitControls
-				enablePan={false}
-				enableZoom={false}
-				minPolarAngle={Math.PI / 2}
-				maxPolarAngle={Math.PI / 2}
-			/>
-		</Canvas>
-	);
+        <Suspense fallback={<div>Loading... </div>}>
+          <ComposedBikeBuild bikeConfig={bikeConfig} />
+        </Suspense>
+        <OrbitControls
+          enablePan={false}
+          enableZoom={false}
+          minPolarAngle={Math.PI / 2.5}
+          maxPolarAngle={Math.PI / 2.5}
+        />
+      </Canvas>
+      <img className="z-0 h-full mx-auto absolute inset-0" src={BikePodium} />
+    </div>
+  );
 };
 
 export default BikeView;
--- app/src/components/pages/BikeBuilder/bikeCanvas/bikeBuild/BikeBuildView.tsx
+++ app/src/components/pages/BikeBuilder/bikeCanvas/bikeBuild/BikeBuildView.tsx
@@ -1,18 +1,18 @@
-import { useEffect } from 'react';
-import { BikeConfig } from '../../../../../types';
-import * as THREE from 'three';
-import BikePart from '../BikePartView';
+import { useEffect } from "react";
+import { BikeConfig } from "../../../../../types";
+import * as THREE from "three";
+import BikePart from "../BikePartView";
 
 interface Props {
-	bikeParts: JSX.Element[];
+  bikeParts: JSX.Element[];
 }
 
 const BikeBuildPresenter = ({ bikeParts }: Props) => {
-	return (
-		<group name="bikeBuild" position={[0, -0.4, 0]}>
-			{bikeParts.map((e) => e)}
-		</group>
-	);
+  return (
+    <group name="bikeBuild" position={[0.2, -0.4, 0]}>
+      {bikeParts.map((e) => e)}
+    </group>
+  );
 };
 
 export default BikeBuildPresenter;
