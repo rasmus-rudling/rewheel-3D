fb8d0c1a682252b72b3d590269e86f71b99035e8
Rasmus Rudling <rasmusrudling@gmail.com>
2021-10-21 12:53:19 +0200

    Workin new merge

--- app/src/components/pages/BikeBuilder/sidePanel/SidePanelPresenter.tsx
+++ app/src/components/pages/BikeBuilder/sidePanel/SidePanelPresenter.tsx
@@ -1,115 +1,115 @@
-import { useEffect, useState } from "react";
-import { Product } from "../../../../types";
-import SidePanelView from "./SidePanelView";
-import { useSelector, useDispatch, RootStateOrAny } from "react-redux";
-import { toggleProductInBuild } from "../../../../redux/actions/currentBuild";
-import { changeProductType } from "../../../../redux/actions/currentProductType";
-import { useMutation, useQuery } from "@apollo/client";
-import { GET_ALL_PRODUCTS } from "../../../../graphql/queries/products";
-import { SAVE_NEW_BIKE } from "../../../../graphql/mutations/bikeBuilds";
-import { useAuth0 } from "@auth0/auth0-react";
-import Spinner from "../../../common/Spinner";
-import { useHistory } from "react-router";
+import { useEffect, useState } from 'react';
+import { Product } from '../../../../types';
+import SidePanelView from './SidePanelView';
+import { useSelector, useDispatch, RootStateOrAny } from 'react-redux';
+import { toggleProductInBuild } from '../../../../redux/actions/currentBuild';
+import { changeProductType } from '../../../../redux/actions/currentProductType';
+import { useMutation, useQuery } from '@apollo/client';
+import { GET_ALL_PRODUCTS } from '../../../../graphql/queries/products';
+import { SAVE_NEW_BIKE } from '../../../../graphql/mutations/bikeBuilds';
+import { useAuth0 } from '@auth0/auth0-react';
+import Spinner from '../../../common/SpinnerView';
+import { useHistory } from 'react-router';
 
 const totNumberOfTypes = 4;
 
 const SidePanelPresenter = () => {
-  const [addBike, addBikeInfo] = useMutation(SAVE_NEW_BIKE);
-  const addBikeLoading = addBikeInfo.loading;
-  const addBikeData = addBikeInfo.data;
+	const [addBike, addBikeInfo] = useMutation(SAVE_NEW_BIKE);
+	const addBikeLoading = addBikeInfo.loading;
+	const addBikeData = addBikeInfo.data;
 
-  const { isAuthenticated, loginWithPopup, user } = useAuth0();
+	const { isAuthenticated, loginWithPopup, user } = useAuth0();
 
-  const { loading, error, data } = useQuery(GET_ALL_PRODUCTS);
+	const { loading, error, data } = useQuery(GET_ALL_PRODUCTS);
 
-  const [allProducts, setAllProducts] = useState<Product[]>([]);
+	const [allProducts, setAllProducts] = useState<Product[]>([]);
 
-  const [productsToShow, setProductsToShow] = useState<Product[]>([]);
+	const [productsToShow, setProductsToShow] = useState<Product[]>([]);
 
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const history = useHistory();
+	const history = useHistory();
 
-  const currentBuild = useSelector(
-    (state: RootStateOrAny) => state.currentBuild
-  );
+	const currentBuild = useSelector(
+		(state: RootStateOrAny) => state.currentBuild
+	);
 
-  const currentProductType = useSelector(
-    (state: RootStateOrAny) => state.currentProductType
-  );
+	const currentProductType = useSelector(
+		(state: RootStateOrAny) => state.currentProductType
+	);
 
-  const productCardClickHandler = (newProductForBuild: Product) => {
-    dispatch(toggleProductInBuild(newProductForBuild));
-  };
+	const productCardClickHandler = (newProductForBuild: Product) => {
+		dispatch(toggleProductInBuild(newProductForBuild));
+	};
 
-  useEffect(() => {
-    const allProductsFromDB = data?.getAllProducts;
+	useEffect(() => {
+		const allProductsFromDB = data?.getAllProducts;
 
-    let allProductsCopy = allProductsFromDB ? [...allProductsFromDB] : [];
+		let allProductsCopy = allProductsFromDB ? [...allProductsFromDB] : [];
 
-    setAllProducts(allProductsCopy);
-  }, [loading]);
+		setAllProducts(allProductsCopy);
+	}, [loading]);
 
-  useEffect(() => {
-    const relevantProducts = allProducts.filter((product) => {
-      return product.type.toLowerCase() === currentProductType.toLowerCase();
-    });
+	useEffect(() => {
+		const relevantProducts = allProducts.filter((product) => {
+			return product.type.toLowerCase() === currentProductType.toLowerCase();
+		});
 
-    setProductsToShow(relevantProducts);
-  }, [currentProductType, allProducts]);
+		setProductsToShow(relevantProducts);
+	}, [currentProductType, allProducts]);
 
-  const currentProductTypeUpdateHandler = (
-    changeTypeOption: "previous" | "next"
-  ) => {
-    dispatch(changeProductType(changeTypeOption, undefined));
-  };
+	const currentProductTypeUpdateHandler = (
+		changeTypeOption: 'previous' | 'next'
+	) => {
+		dispatch(changeProductType(changeTypeOption, undefined));
+	};
 
-  const saveBikeHandler = () => {
-    if (!isAuthenticated) {
-      loginWithPopup();
-      return;
-    }
-    if (currentBuild.products.length !== 4) return;
+	const saveBikeHandler = () => {
+		if (!isAuthenticated) {
+			loginWithPopup();
+			return;
+		}
+		if (currentBuild.products.length !== 4) return;
 
-    const productIDs: Product[] = currentBuild.products.map(
-      (product: Product) => product.product_id
-    );
+		const productIDs: Product[] = currentBuild.products.map(
+			(product: Product) => product.product_id
+		);
 
-    if (isAuthenticated && user) {
-      addBike({
-        variables: {
-          email: user.email,
-          products: productIDs,
-          createdBy: user.email,
-          createdAt: new Date().toUTCString(),
-        },
-      });
-    }
-  };
+		if (isAuthenticated && user) {
+			addBike({
+				variables: {
+					email: user.email,
+					products: productIDs,
+					createdBy: user.email,
+					createdAt: new Date().toUTCString(),
+				},
+			});
+		}
+	};
 
-  if (addBikeLoading) {
-    return (
-      <div className="h-full flex justify-center items-center">
-        <Spinner />
-      </div>
-    );
-  }
+	if (addBikeLoading) {
+		return (
+			<div className="h-full flex justify-center items-center">
+				<Spinner />
+			</div>
+		);
+	}
 
-  if (addBikeData) {
-    history.push("profile");
-  }
+	if (addBikeData) {
+		history.push('profile');
+	}
 
-  return (
-    <SidePanelView
-      currentBuild={currentBuild}
-      currentProductCards={productsToShow}
-      productCardClickHandler={productCardClickHandler}
-      currentProductTypeUpdateHandler={currentProductTypeUpdateHandler}
-      currentProductType={currentProductType}
-      totNumberOfTypes={totNumberOfTypes}
-      saveBikeHandler={saveBikeHandler}
-    />
-  );
+	return (
+		<SidePanelView
+			currentBuild={currentBuild}
+			currentProductCards={productsToShow}
+			productCardClickHandler={productCardClickHandler}
+			currentProductTypeUpdateHandler={currentProductTypeUpdateHandler}
+			currentProductType={currentProductType}
+			totNumberOfTypes={totNumberOfTypes}
+			saveBikeHandler={saveBikeHandler}
+		/>
+	);
 };
 
 export default SidePanelPresenter;
