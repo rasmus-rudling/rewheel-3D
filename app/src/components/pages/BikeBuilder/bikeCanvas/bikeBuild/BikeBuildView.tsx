2618f43aa5a16cd0ab7f295cf3cbf82f75708816
Rasmus Rudling <rasmusrudling@gmail.com>
2021-11-02 15:32:08 +0100

    Added key to BikeBuildView

--- app/src/components/pages/BikeBuilder/bikeCanvas/bikeBuild/BikeBuildView.tsx
+++ app/src/components/pages/BikeBuilder/bikeCanvas/bikeBuild/BikeBuildView.tsx
@@ -1,18 +1,20 @@
-import { useEffect } from "react";
-import { BikeConfig } from "../../../../../types";
-import * as THREE from "three";
-import BikePart from "../BikePartView";
+import { useEffect } from 'react'
+import { BikeConfig } from '../../../../../types'
+import * as THREE from 'three'
+import BikePart from '../BikePartView'
 
 interface Props {
-  bikeParts: JSX.Element[];
+	bikeParts: JSX.Element[]
 }
 
-const BikeBuildPresenter = ({ bikeParts }: Props) => {
-  return (
-    <group name="bikeBuild" position={[0.2, -0.4, 0]}>
-      {bikeParts.map((e) => e)}
-    </group>
-  );
-};
+const BikeBuildView = ({ bikeParts }: Props) => {
+	return (
+		<group name="bikeBuild" position={[0.2, -0.4, 0]}>
+			{bikeParts.map((bikePart, idx) => {
+				return <group key={idx}>{bikePart}</group>
+			})}
+		</group>
+	)
+}
 
-export default BikeBuildPresenter;
+export default BikeBuildView
